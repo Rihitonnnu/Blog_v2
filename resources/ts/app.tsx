0c3389433481807bb0b16c0ec555a1c9ffed6cd9
Rihitonnnu <hexiliang04@gmail.com>
2023-03-12 15:11:35 +0900

    fix: パスなどviteの設定

--- resources/ts/app.tsx
+++ resources/ts/app.tsx
@@ -1,16 +1,32 @@
-import React from "react";
-import { render } from "react-dom";
-import { createInertiaApp } from "@inertiajs/inertia-react";
-import { resolvePageComponent } from "laravel-vite-plugin/inertia-helpers";
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { createInertiaApp } from '@inertiajs/inertia-react';
+import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 
 createInertiaApp({
     title: (title) => title,
     resolve: (name) =>
         resolvePageComponent(
             `./Pages/${name}.tsx`,
-            import.meta.glob("./Pages/**/*.tsx")
+            import.meta.glob('./Pages/**/*.tsx')
         ),
     setup({ el, App, props }) {
-        render(<App {...props} />, el);
-    },
+        const {
+            initialPage,
+            initialComponent,
+            resolveComponent,
+            titleCallback,
+            onHeadUpdate
+        } = props;
+        const root = createRoot(el);
+        return root.render(
+            <App
+                initialPage={initialPage}
+                initialComponent={initialComponent}
+                resolveComponent={resolveComponent}
+                titleCallback={titleCallback}
+                onHeadUpdate={onHeadUpdate}
+            />
+        );
+    }
 });
